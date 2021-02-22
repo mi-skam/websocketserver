cde6f1ce6b5a4dbda7632bc6f7106ea88283e45e
mi-skam <maksim.codes@mailbox.org>
2021-02-22 03:06:25 +0100

    added control channel

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const io = socketIO(server);
 app.use(express.static('public'));
 
 io.sockets.on('connection', (socket) => {
-  console.log(socket.id);
-
-  socket.on('control', (data) => console.log(`Received: ${data}`));
+    socket.on('control', (data) => console.log(`Received: ${data.id}`));
 })
 
 
